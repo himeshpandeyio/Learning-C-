616873725621b6627de9fa22f2b9dae35d3ae1ca
Himesh <pandeyhimesh65@gmail.com>
2026-07-21 15:04:00 +0545

    Updated program running successfully

---
+++ main/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class MainProgram
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+        string NAME =  .ToUpper();
+        string Name = NAME.Insert(2,"@@");
+        Console.WriteLine("Hello! " + Name + ".");
+    }
+}
+
