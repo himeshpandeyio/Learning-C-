e3c48eaf25a4311707a508afd7cc295b90bc9195
Himesh <pandeyhimesh65@gmail.com>
2026-08-05 21:54:17 +0545

    Updated program running successfully

--- main/Program.cs
+++ main/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("your name: ");
-        string? name = Console.ReadLine();
-        Console.WriteLine("Hello World");
-        Console.WriteLine("I'm " + name);
+using System.Runtime.InteropServices;
 
+namespace main
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("Its a prime number");
+            }
+            else if(number == 1)
+            {
+                Console.WriteLine("1 cannot be a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("It's not a prime number.");
+            }
+        }
     }
 }
