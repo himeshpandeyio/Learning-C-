106d57c21d71e380b07acf1c274481ffa689250f
Himesh <pandeyhimesh65@gmail.com>
2026-08-05 22:54:29 +0545

    Updated program running successfully

--- main/Program.cs
+++ main/Program.cs
@@ -1,28 +1,54 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
-namespace main
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the number : ");
-            int number;
-            number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Simple Calculator");
+        Console.WriteLine("------------------");
 
-            if (number % 2 != 0)
-            {
-                Console.WriteLine("Its a prime number");
-            }
-            else if(number == 1)
-            {
-                Console.WriteLine("1 cannot be a prime number.");
-            }
-            else
-            {
-                Console.WriteLine("It's not a prime number.");
-            }
+        Console.Write("Enter first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("First Number: " + firstNumber);
+        Console.WriteLine("Second NUmber: " + secondNumber);
+
+        Console.WriteLine();
+        Console.WriteLine("Choose an operation: ");
+        Console.WriteLine("1.Add");
+        Console.WriteLine("2.Subtract");
+        Console.WriteLine("3.Multiply");
+        Console.WriteLine("4.Divide");
+
+        Console.WriteLine("Enter your choice: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("sum: " + (firstNumber + secondNumber));
+                break;
+
+            case 2:
+                Console.WriteLine("difference: " + (firstNumber + secondNumber));
+                break;
+
+            case 3:
+                Console.WriteLine("product: " + (firstNumber + secondNumber));
+                break;
+
+            case 4:
+                Console.WriteLine("Quotient: " + (firstNumber / secondNumber));
+                break;
+
+            default:
+                Console.WriteLine("Invalid Choice!");
+                break;    
         }
+
     }
 }
